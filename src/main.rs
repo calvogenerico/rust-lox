73a8f7fa06327b7724e4cd57c752a5ebf62a26cf
calvo.generico <miguelfeliped@gmail.com>
2024-11-10 08:17:48 -0300

    lint

--- src/main.rs
+++ src/main.rs
@@ -11,45 +11,44 @@ use crate::scanner::Scanner;
 #[command(name = "git")]
 #[command(about = "A fictional versioning CLI", long_about = None)]
 struct Cli {
-    #[command(subcommand)]
-    command: Commands,
+  #[command(subcommand)]
+  command: Commands,
 }
 
 
 #[derive(Debug, Subcommand)]
 enum Commands {
-    /// Clones repos
-    #[command(arg_required_else_help = true)]
-    Tokenize {
-        file_path: Option<String>,
-    }
+  /// Clones repos
+  #[command(arg_required_else_help = true)]
+  Tokenize {
+    file_path: Option<String>,
+  }
 }
 
 fn main() -> Result<ExitCode, Error> {
-    let args = Cli::parse();
+  let args = Cli::parse();
 
-    let code = match args.command {
-        Commands::Tokenize { file_path: Some(path) } => {
-            let mut input = File::open(&path)?;
-            let mut scanner = Scanner::new(&mut input);
-            let (tokens, errors) = scanner.scan_tokens();
+  let code = match args.command {
+    Commands::Tokenize { file_path: Some(path) } => {
+      let mut input = File::open(&path)?;
+      let mut scanner = Scanner::new(&mut input);
+      let (tokens, errors) = scanner.scan_tokens();
 
-            for error in &errors {
-                eprintln!("{error}")
-            }
+      for error in &errors {
+        eprintln!("{error}")
+      }
 
-            for token in tokens {
-                println!("{}", token.to_string());
-            }
+      for token in tokens {
+        println!("{}", token.to_string());
+      }
 
-            if errors.len() == 0 {
-                ExitCode::from(0)
-            } else {
-                ExitCode::from(65)
-            }
-
-        },
-        _ => ExitCode::from(1)
-    };
-    Ok(code)
+      if errors.len() == 0 {
+        ExitCode::from(0)
+      } else {
+        ExitCode::from(65)
+      }
+    }
+    _ => ExitCode::from(1)
+  };
+  Ok(code)
 }
